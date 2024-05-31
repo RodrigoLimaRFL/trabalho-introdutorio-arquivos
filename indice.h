410b2c9803382b78477189cd60bb816c5be0941b
Pedro Henrique <pedrohenriquefesi@gmail.com>
2024-05-31 18:23:32 -0300

    hfiehfie

--- indice.h
+++ indice.h
@@ -8,19 +8,6 @@
 
         typedef struct _lista LISTA_INDICE;
 
-<<<<<<< HEAD
-        LISTA_INDICE *criarLista();
-        REGISTRO_INDICE *getRegistro(LISTA_INDICE *lista, int index);
-        int getTamanho(LISTA_INDICE *lista);
-        bool adicionarRegistro(LISTA_INDICE *lista, REGISTRO_INDICE *registro);
-        bool adicionarRegistroOrdenado(LISTA_INDICE *lista, REGISTRO_INDICE *registro, FILE *file);
-        bool modificarRegistro(LISTA_INDICE *lista, int index, REGISTRO_INDICE *novoRegistro);
-        REGISTRO_INDICE *buscarRegistro(LISTA_INDICE *lista, int id);
-        void removerRegistro(LISTA_INDICE *lista, int index);
-        bool apagarLista(LISTA_INDICE *lista);
-        void imprimirLista(LISTA_INDICE *lista);
-        REGISTRO *buscarRegistroOffset(long long offset, FILE *file);
-=======
         LISTA_INDICE *criarListaIndice();
         REGISTRO_INDICE *getRegistroIndice(LISTA_INDICE *lista, int index);
         int getTamanhoIndice(LISTA_INDICE *lista);
@@ -32,6 +19,5 @@
         bool apagarListaIndice(LISTA_INDICE *lista);
         void imprimirListaIndice(LISTA_INDICE *lista);
         bool carregarIndice(LISTA_INDICE *lista, FILE *file);
->>>>>>> refs/remotes/origin/main
 #endif
 
