5d6c7abbd43ddfae95e91720889c18966ff3afd0
Pedro Henrique <pedrohenriquefesi@gmail.com>
2024-05-31 16:41:04 -0300

    removidos

--- removidos.c
+++ removidos.c
@@ -2,13 +2,14 @@
 
 struct _removidos {
   LISTA_INDICE *lista;
-  int *tamanhoRegistro;
+  int tamanhoRegistro[1000];
 };
 
 REMOVIDOS *criarListaRemovidos(char *filePath) {
   CABECALHO *cabecalho = getCabecalhoFromBin2(filePath);
 
-  REMOVIDOS *removidos = criarLista();
+  REMOVIDOS *removidos;
+  removidos->lista = criarLista();
   FILE *file = fopen(filePath, "rb");
 
   if (file == NULL) // verifica se ocorreu um erro ao abrir o arquivo no modo leitura
@@ -17,15 +18,19 @@ REMOVIDOS *criarListaRemovidos(char *filePath) {
     return NULL;
   }
 
-  int proxyByteOffset = getProxByteOffset(cabecalho);
+  int proxyByteOffset = getTopo(cabecalho);
   fseek(file, proxyByteOffset, SEEK_SET);
 
+  int i=0;
   while(proxyByteOffset != -1) {
     REGISTRO *registro = criarRegistroNulo(); // cria um registro com os valores iniciais
     lerRegistroFromBin2(file, registro); // salva os valores do registro do arquivo binário no registro criado
     
-    adicionarRegistro(removidos, registro);
-    fseek(file, get_prox(registro), SEEK_SET);
+    adicionarRegistro(removidos->lista, registro);
+    proxyByteOffset = get_prox(registro);
+    fseek(file, proxyByteOffset, SEEK_SET);
+    removidos->tamanhoRegistro[i] = get_tamanhoRegistro(registro);
+    i++;
   }
 
   return removidos;
