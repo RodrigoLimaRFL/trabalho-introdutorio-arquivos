7e1baf1a735797b0b0a3122bc5232e297a9526be
Rodrigo Lima <guico21@usp.br>
2024-06-21 10:46:05 -0300

    registro arvore b

---
+++ registroArvoreB.c
@@ -0,0 +1,6 @@
+struct _registroArvoreB {
+    int alturaNo;
+    int nroChaves;
+    int *chaves;
+    long long int *ponteiros;
+};
---
+++ registroArvoreB.h
@@ -0,0 +1,10 @@
+#ifndef REGISTROARVOREB_H
+    #define REGISTROARVOREB_H
+
+    #include <stdlib.h>
+    #include <stdio.h>
+
+    #define ORDEM_ARVORE_B 4
+    
+    typedef struct _registroArvoreB REGISTRO_ARVORE_B;
+#endif
